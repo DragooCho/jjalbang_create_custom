0ed80652bdc81a3103ee7cf97ff07af00ec68472
dragoocho <deagoocho@gmail.com>
2021-03-15 09:09:31 +0900

    스케치 테스트

--- src/Utility/sketch.js
+++ src/Utility/sketch.js
@@ -1,36 +1,33 @@
 import React from "react";
 import Sketch from "react-p5";
 
-const sketch = () => {
-  let canvas;
+const sketch = (p) => {
+  let input;
   let img;
 
-  function setup(p, canvasParentRef) {
-    canvas = p.createCanvas(300, 200).parent(canvasParentRef);
-    canvas.drop(gotFile);
-    canvas.mousePressed(inputTextPaint);
-    p.background(180, 0, 180);
+  function setup(p) {
+    input = p.createFileInput();
+
+    input.position(0, 0);
+    p.createCanvas(400, 400);
   }
 
-  function gotFile(p, file) {
-    img = p.loadImage(file.data, imageView);
-  }
-
-  function imageView(p) {
-    canvas.resize(img.width, img.height);
-    p.image(img, 0, 0, p.width, p.height);
-  }
-
-  function inputTextPaint(p) {
-    p.textSize(50); // 사이즈 조절
+  function draw(p) {
+    p.textSize(32);
     p.textAlign(p.CENTER, p.CENTER);
-    p.textStyle(p.BOLD); // 스타일 지정, NORMAL, ITALIC, BOLD, BOLDITALIC 를 설정 가능
-    p.text("Font Size 16", p.mouseX, p.mouseY); // 폰트의 내용, 해당 마우스 위치에 그려준다.
+    p.background(220);
+    if (img) {
+      p.image(img, 0, 0, p.width, p.height);
+    }
+    p.text("word", p.mouseX, p.mouseY);
+    if (p.mouseIsPressed) {
+      p.noLoop();
+    }
   }
 
   return (
     <div>
-      <Sketch setup={setup} />
+      <Sketch setup={setup} draw={draw} />
     </div>
   );
 };
